1db02ef7e6e297761a77e3940d22a15f493ba30f
duygu <duyguonaral@gmail.com>
2018-12-14 13:38:24 +0300

    beforeEach for autonomous tests

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,7 +10,7 @@ function AuthController(){
     }
 
     function isAuthorisedAsync(neededRole, cb) {
-        setTimeout(function(){cb(roles.indexOf(neededRole) >= 0)}, 2100);
+        setTimeout(function(){cb(roles.indexOf(neededRole) >= 0)}, 0);
     }
 
     return {isAuthorised, isAuthorisedAsync, setRoles};
--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,10 +1,13 @@
 var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
 
-describe('AuthController', function(){
-    describe('isAuthorised', function(){
+describe('AuthController', function () {
+    beforeEach(function () {
+        console.log('running before each');
+        authController.setRoles(['user']);
+    });
+    describe('isAuthorised', function () {
         it('should return false if not authorised', function(){
-            authController.setRoles(['user']);
             assert.equal(false, authController.isAuthorised('admin'));
         })
         it('should return true if authorised', function(){
@@ -14,8 +17,6 @@ describe('AuthController', function(){
     })
     describe('isAuthorisedAsync', function(){
         it('should return false if not authorised', function(done){
-            this.timeout(2500);
-            authController.setRoles(['user']);
             authController.isAuthorisedAsync('admin',
                 function(isAuth){
                     assert.equal(false, isAuth);
