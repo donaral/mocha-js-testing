5d50e6e470c4b77891b4a3812d8bee39c4a40e41
duygu <duyguonaral@gmail.com>
2018-12-14 13:27:19 +0300

    setRoles not DRY

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,19 @@
 function AuthController(){
     
-    function isAuthorised(roles, neededRole) {
+    var roles;
+    function setRoles(role){
+        roles = role;
+    }
+
+    function isAuthorised(neededRole) {
         return roles.indexOf(neededRole) >= 0;
     }
 
-    function isAuthorisedAsync(roles, neededRole, cb) {
+    function isAuthorisedAsync(neededRole, cb) {
         setTimeout(function(){cb(roles.indexOf(neededRole) >= 0)}, 2100);
     }
 
-    return {isAuthorised, isAuthorisedAsync};
+    return {isAuthorised, isAuthorisedAsync, setRoles};
 }
 
 module.exports = AuthController();
--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -4,16 +4,19 @@ var authController = require('../../controllers/auth.controller');
 describe('AuthController', function(){
     describe('isAuthorised', function(){
         it('should return false if not authorised', function(){
-            assert.equal(false, authController.isAuthorised(['user'], 'admin'));
+            authController.setRoles(['user']);
+            assert.equal(false, authController.isAuthorised('admin'));
         })
-        it('should return true if not authorised', function(){
-            assert.equal(true, authController.isAuthorised(['user', 'admin'], 'admin'));
+        it('should return true if authorised', function(){
+            authController.setRoles(['user', 'admin']);
+            assert.equal(true, authController.isAuthorised('admin'));
         })
     })
     describe('isAuthorisedAsync', function(){
         it('should return false if not authorised', function(done){
             this.timeout(2500);
-            authController.isAuthorisedAsync(['user'], 'admin',
+            authController.setRoles(['user']);
+            authController.isAuthorisedAsync('admin',
                 function(isAuth){
                     assert.equal(false, isAuth);
                     done();
