ab900c4963c0acc0efe08007391ef35b5e4a4811
duygu <duyguonaral@gmail.com>
2018-12-18 16:17:02 +0300

    should null problem

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var should = require('chai').should();
 
 describe('basic mocha test', function(){
     it('should deal with objects', function(){
@@ -7,5 +8,9 @@ describe('basic mocha test', function(){
 
         obj.should.have.property('name').equal('Duygu');
         obj.should.deep.equal(objB);
+    });
+    it('should allow testing nulls', function(){
+        var iAmNull = null;
+        should.not.exist(iAmNull);
     })
 });
